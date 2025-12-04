731af03f08d9cd519286d529df130726d290fe33
VDRM12 <valentinadanielarojasmendoza@gmail.com>
2025-12-04 11:51:40 -0500

    update: page and create functions test

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,83 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+
+type Usuario = {
+  nombre: String,
+  edad: number
+}
 
 export default function Home() {
+  const [valor, setValor] = useState<string>("");
+  const [resultado, setResultado] = useState<string>("");
+  const evaluarNumero = (num:number) => {
+    if(num > 0) return 'El numero es positivo'
+    if(num < 0) return 'El numero es negativo'
+    return 'Numero es 0'
+  }
+  const manejarcambio = (e: React.ChangeEvent<HTMLInputElement >) => {
+    const numero = Number(e.target.value);
+    setValor(e.target.value)
+    if(!isNaN(numero)){
+      setResultado(evaluarNumero(numero))
+    }
+    else{
+      setResultado("Ingresa un numero valido")
+    }
+  }
+
+  const usuarios: Usuario[] = [
+    {nombre: "Ana", "edad": 22},
+    {nombre: "Luis", "edad": 17},
+    {nombre: "Carlos", "edad": 30} 
+  ]
+    const filtrarMayores = (lista:Usuario[]) : Usuario[] => {
+      return lista.filter((usuario) => usuario.edad > 18)
+    }
+
+
+
+    const [email,setEmail] = useState();
+    const [clave,setClave] = useState();
+    const handleSubmit =(e: React.FormEvent)=>{
+      alert("Inicio exitoso")
+    }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+       <h3>ejercicio  1</h3>
+        <input value={valor} onChange={manejarcambio} placeholder="ingresar numero"/>
+        <p>resultado: {resultado}</p>
+
+        <h3>Ejercicio 2</h3>
+        <div>
+            {filtrarMayores(usuarios).map((usuario) =>(
+              <li>{usuario.nombre}  -  {usuario.edad} años</li>
+            )) }
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+            
+            <h3>Ejercicio 3 </h3>
+            <form onSubmit={handleSubmit}>
+              <input
+                value={email}
+                type="email"
+                required
+                className="border-gray-300 focus:border-blue-500 focus:ring-blue-500
+                      invalid:border-red-500 invalid:text-red-600
+                      valid:border-green-500 valid:text-green-600"
+                placeholder="Email"
+              />
+              <input
+                value={clave}
+                type="password"
+                required
+                className="border-gray-300 focus:border-blue-500 focus:ring-blue-500
+                      invalid:border-red-500 invalid:text-red-600
+                      valid:border-green-500 valid:text-green-600"
+                placeholder="Contraseña"
+              />
+              <button type="submit"   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ingresar</button>
+            </form>
+      </main> 
     </div>
   );
 }
